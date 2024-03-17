fc29e1723378921a8998f71dfaf2ec992c5cc8b7
strategicbonds <strategicbonds@gmail.com>
2024-03-17 12:43:07 -0400

    added Components empty files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        baddass cars for baller prices
+        a data driven car buying experience
         </p>
         <a
           className="App-link"
           href="https://frontend-yellow-rhino-d42f892b13c8.herokuapp.com/"
           target="_blank"
-          rel="Badass cars for baller prices"
+          rel="a data driven car buying experience"
         >
           Yellow Rhino
         </a>
