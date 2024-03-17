178529432aca7deda5b8ed6093a52a4c47ce9fd6
strategicbonds <strategicbonds@gmail.com>
2024-03-17 15:33:31 -0400

    first component functioning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
+import React from 'react';
 import logo from './logo.png';
 import './App.css';
+import Header from './Components/HeaderOne'; // Adjust the path if necessary
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
+      <Header />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        a data driven car buying experience
+          a data driven car buying experience
         </p>
         <a
           className="App-link"
           href="https://frontend-yellow-rhino-d42f892b13c8.herokuapp.com/"
           target="_blank"
-          rel="a data driven car buying experience"
+          rel="noopener noreferrer" // Fix the rel attribute
         >
           Yellow Rhino
         </a>
-      </header>
+      </div>
     </div>
   );
 }
